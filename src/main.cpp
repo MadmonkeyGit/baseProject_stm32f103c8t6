c0b9056625b024b7bf1a61af49b13af945c55d49
MadmokeyGit <leonhard.raschein@gmail.com>
2021-02-27 14:09:54 +0100

    added PWM

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,46 @@ bool rise = true;
 
 void ISR_timerNr1();
 
+HardwareTimer *timerPWM;
+HardwareTimer *timerISR;
+
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  TIM_TypeDef *Instance = TIM4;
-  HardwareTimer *timer = new HardwareTimer(Instance);
-  timer->setOverflow(2000000, MICROSEC_FORMAT);
-  timer->attachInterrupt(ISR_timerNr1);
-  timer->resume();
+  pinMode(PB9, OUTPUT);
+
+  timerISR = new HardwareTimer(TIM4);
+  timerISR->setOverflow(2000, MICROSEC_FORMAT);
+  timerISR->attachInterrupt(ISR_timerNr1);
+  timerISR->resume();
+
+  timerPWM = new HardwareTimer(TIM1);
+  timerPWM->setOverflow(1000, HERTZ_FORMAT); 
+  timerPWM->setMode(2, TIMER_OUTPUT_COMPARE_PWM2, PA9);
+  timerPWM->setCaptureCompare(2, 0, PERCENT_COMPARE_FORMAT);
+  timerPWM->resume();
 }
 
 void loop()
 {
-  delay(10);
+  for (int i=0; i<=100; i++)
+  {
+    timerPWM->setCaptureCompare(2, i, PERCENT_COMPARE_FORMAT);
+    delay(5);
+  }
+  for (int i=100; i>=0; i--)
+  {
+    timerPWM->setCaptureCompare(2, i, PERCENT_COMPARE_FORMAT);
+    delay(5);
+  }
 }
 
 void ISR_timerNr1(){
   if (rise){
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(PB9, HIGH);
     rise = false;
   }
   else{
-    digitalWrite(ledPin, LOW);
+    digitalWrite(PB9, LOW);
     rise = true;
   }
 }
